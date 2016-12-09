a7b5c028bef244edf10f90c529b862cb249966e2
s4900092 <s4900092@bournemouth.ac.uk>
2016-12-09 11:20:27 +0000

    new folder: shaders

---
+++ Grid/shaders/gridFragment.glsl
@@ -0,0 +1,10 @@
+# version 420 core
+
+layout (location = 0) out vec4 fragColor;
+uniform vec4 color;
+in vec3 lightIntensity;
+void main()
+{
+    //set everything to white
+    fragColor.rgb = lightIntensity;
+}
